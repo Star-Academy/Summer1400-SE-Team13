47fde47c7f3f764c28f460510bf45349c65e4ef9
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-07-24 22:23:35 +0430

    Sorted IDs before printing

--- Phase1/Main.java
+++ Phase1/Main.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 public class Main {
     static void run(String searchWord) {
-        GetInput getInput = new GetInput("home/melika/Desktop/codes/codestar/phase1/EnglishData");
+        GetInput getInput = new GetInput("C:/Users/ASUS/Downloads/SampleEnglishData/EnglishData");
         HashMap<Integer, String> hashMap = getInput.readContent();
         InvertedIndex invertedIndex = new InvertedIndex();
-        
-        for (int key : hashMap.keySet()) {
-            String docString = hashMap.get(key);
+
+        for (int id : hashMap.keySet()) {
+            String docString = hashMap.get(id);
             Tokenizer tokenizer = new Tokenizer(docString);
             HashSet<String> wordsSet = tokenizer.tokenize();
-            invertedIndex.addDoc(wordsSet, key);
+            invertedIndex.addDoc(wordsSet, id);
         }
 
         HashSet<Integer> foundDocs = invertedIndex.getWordDocs(searchWord);
-        System.out.println(foundDocs);
+        TreeSet<Integer> sortedDocs = new TreeSet<Integer>(foundDocs);
+        System.out.println(sortedDocs);
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String searchWord = scanner.nextLine();
