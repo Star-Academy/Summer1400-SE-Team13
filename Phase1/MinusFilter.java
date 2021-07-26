5988954aacfee87bf0946a242ffdfee50229218e
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-07-26 11:38:31 +0430

    Refactor filter method

--- Phase1/MinusFilter.java
+++ Phase1/MinusFilter.java
@@ -2,9 +2,8 @@ import java.util.HashSet;
 
 public class MinusFilter implements Filter {
     public HashSet<Integer> filter(HashSet<Integer> result, HashSet<Integer> docs) {
-        if (docs == null)
-            return result;
-        result.removeAll(docs);
+        if (docs != null)
+            result.removeAll(docs);
         return result;
     }
 }
