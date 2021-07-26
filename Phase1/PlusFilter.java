9d5ed0cf911d0f932ec621f54347efd87a72abb9
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-07-26 11:42:14 +0430

    Refactor filter method

--- Phase1/PlusFilter.java
+++ Phase1/PlusFilter.java
@@ -2,9 +2,8 @@ import java.util.HashSet;
 
 public class PlusFilter implements Filter {
     public HashSet<Integer> filter(HashSet<Integer> result, HashSet<Integer> docs) {
-        if (docs == null)
-            return result;
-        result.addAll(docs);
+        if (docs != null)
+            result.addAll(docs);
         return result;
     }
 }
