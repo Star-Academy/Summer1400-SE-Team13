06ff73484c12f764ae2b83c938bdc25a24409683
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-07-27 19:41:53 +0430

    Update Main.java

--- Phase1/Main.java
+++ Phase1/Main.java
@@ -2,11 +2,22 @@ import java.util.*;
 
 public class Main {
 
+    static void printResult(HashSet<Integer> result) {
+        if (result.isEmpty())
+            System.out.println("no doc found!");
+        else {
+            System.out.println(result.size());
+            System.out.println(result);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine().toLowerCase();
-        FullTextSearch fullTextSearch = new FullTextSearch(command);
-        fullTextSearch.run();
+        InvertedIndex invertedIndex = new InvertedIndex();
+        FullTextSearch fullTextSearch = new FullTextSearch(command, invertedIndex);
+        HashSet<Integer> result = fullTextSearch.run();
+        printResult(result);
         scanner.close();
     }
 }
