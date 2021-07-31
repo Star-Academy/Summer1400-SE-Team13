178138da3badd5f97333eff4813ed85e0da65140
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-07-31 11:56:44 +0430

    Fixed bug

--- Phase1/src/NoSignFilter.java
+++ Phase1/src/NoSignFilter.java
@@ -6,6 +6,7 @@ public class NoSignFilter implements Filter {
     public void filter(HashSet<Integer> result, HashSet<Integer> docs) {
         if (docs != null)
             result.retainAll(docs);
-        result.clear();
+        else
+            result.clear();
     }
 }
