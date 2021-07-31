633b581f962a2edf8292ef5cd595245df4659499
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-07-31 09:47:11 +0430

    Refactor InvertedIndexTest.java

--- Phase1/test/InvertedIndexTest.java
+++ Phase1/test/InvertedIndexTest.java
@@ -2,48 +2,37 @@ package test;
 
 import src.*;
 import java.util.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class InvertedIndexTest {
 
-    private static InvertedIndex invertedIndex = new InvertedIndex();
-    private static HashMap<String, HashSet<Integer>> testWordsMap = new HashMap<>();;
+    private static final InvertedIndex invertedIndex = new InvertedIndex();
+    private static final HashMap<String, HashSet<Integer>> testWordsMap = new HashMap<>();
 
-    @BeforeAll
-    static void initWordsmap() {
-        HashSet<Integer> h1 = new HashSet<>();
-        h1.add(1);
-        HashSet<Integer> h2 = new HashSet<>();
-        h2.add(1);
-        HashSet<Integer> h3 = new HashSet<>();
-        h3.add(1);
+    private void initWordsMap() {
+        HashSet<Integer> h1 = new HashSet<>(Collections.singletonList(1));
+        HashSet<Integer> h2 = new HashSet<>(Collections.singletonList(1));
+        HashSet<Integer> h3 = new HashSet<>(Collections.singletonList(1));
         testWordsMap.put("str1", h1);
         testWordsMap.put("str2", h2);
         testWordsMap.put("str3", h3);
 
-        HashSet<String> docWords = new HashSet<>();
-        docWords.add("str1");
-        docWords.add("str2");
-        docWords.add("str3");
+        HashSet<String> docWords = new HashSet<>(Arrays.asList("str1", "str2", "str3"));
         int docId = 1;
         invertedIndex.addDoc(docWords, docId);
     }
 
     @Test
     public void testAddDocs() {
-
+        initWordsMap();
         assertEquals(testWordsMap, invertedIndex.getWordsMap());
     }
 
     @Test
     public void testGetWordDocs() {
-        System.out.println(testWordsMap);
-        HashSet<Integer> wordDocs = new HashSet<>(Arrays.asList(1));
-        HashSet<Integer> tmp = invertedIndex.getWordDocs("str1");
-
-        System.out.println(tmp);
-        assertEquals(wordDocs, tmp);
+        initWordsMap();
+        HashSet<Integer> wordDocs = new HashSet<>(Collections.singletonList(1));
+        assertEquals(wordDocs, invertedIndex.getWordDocs("str1"));
     }
 }
