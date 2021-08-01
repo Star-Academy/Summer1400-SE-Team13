e6e81bc8390de4942edfa7ea2d652a679472b23b
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-01 13:16:10 +0430

    Updated

--- phase04/Grade.cs
+++ phase04/Grade.cs
@@ -2,11 +2,9 @@ namespace phase04
 {
     class Grade 
     {   
-        
         public int StudentNumber {get; set;}
         public string Lesson {get; set;}
         public double Score {get; set;}
-
         public Grade(int studentNumber, string lesson, double score)
         {
             Score = score;
