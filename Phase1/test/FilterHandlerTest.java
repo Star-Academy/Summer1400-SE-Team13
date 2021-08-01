969801d4abcb31da6261413712b4e31c828ab315
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-01 17:02:49 +0430

    Use mock

--- Phase1/test/FilterHandlerTest.java
+++ Phase1/test/FilterHandlerTest.java
@@ -1,29 +1,44 @@
 package test;
 
+import org.junit.Before;
 import src.*;
 import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 
 public class FilterHandlerTest {
-    private InvertedIndex invertedIndex = new InvertedIndex();
-    private PlusFilter plusFilter = new PlusFilter();
-    private NoSignFilter noSignFilter = new NoSignFilter();
-    private MinusFilter minusFilter = new MinusFilter();
+    @Mock
+    PlusFilter plusFilter;
+    @Mock
+    MinusFilter minusFilter;
+    @Mock
+    NoSignFilter noSignFilter;
+    @Mock
+    InvertedIndex invertedIndex;
 
-    public void initializeInvertedIndex() {
-        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s2", "s3")), 1);
-        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s4", "s5")), 2);
-        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s4", "s6")), 3);
+    @Before
+    public void setup() {
+//        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s2", "s3")), 1);
+//        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s4", "s5")), 2);
+//        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s4", "s6")), 3);
+        Mockito.when(invertedIndex.getWordDocs("s1")).thenReturn(new HashSet<>(Arrays.asList(1,2)));
+        Mockito.when(invertedIndex.getWordDocs("s2")).thenReturn(new HashSet<>(Collections.singleton(1)));
+        Mockito.when(invertedIndex.getWordDocs("s3")).thenReturn(new HashSet<>(Collections.singleton(1)));
+        Mockito.when(invertedIndex.getWordDocs("s4")).thenReturn(new HashSet<>(Collections.singleton(2)));
+
+
+
     }
 
     @Test
     public void testFilter() {
-        initializeInvertedIndex();
         FilterHandler filterHandler = new FilterHandler(invertedIndex, plusFilter, noSignFilter, minusFilter);
         String[] commandWords = { "+s1", "-s2", "s6" };
         HashSet<Integer> actualResult = filterHandler.filter(commandWords);
-        HashSet<Integer> excpectedResult = new HashSet<>();
-        assertEquals(excpectedResult, actualResult);
+        HashSet<Integer> expectedResult = new HashSet<>();
+        assertEquals(expectedResult, actualResult);
     }
 }
