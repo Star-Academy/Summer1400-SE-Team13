a52677d26e22a3f98c50a81373d06673e010251a
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-01 22:18:14 +0430

    Updated

--- Phase1/test/FullTextSearchTest.java
+++ Phase1/test/FullTextSearchTest.java
@@ -16,7 +16,7 @@ public class FullTextSearchTest {
     private NoSignFilter noSignFilter;
 
     @Before
-    public void initialize() {
+    public void setup() {
         invertedIndex = new InvertedIndex();
         tokenizer = new Tokenizer();
         docsFileReader = new DocsFileReader();
@@ -28,22 +28,9 @@ public class FullTextSearchTest {
 
     @Test
     public void testFullTextSearch() {
-        initialize();
-        // ArrayList<String> tests = new ArrayList<>(
-        // Arrays.asList("+hello -bye how", "Hello", "-bye", "-Raha -melika", "test"));
-        // ArrayList<HashSet<Integer>> answers = new ArrayList<>(Arrays.asList(new
-        // HashSet<>(),
-        // new HashSet<>(Arrays.asList(1, 2)), new HashSet<>(), new HashSet<>(), new
-        // HashSet<>()));
-        // for (int i = 0; i < 3; i++) {
-        // FullTextSearch fullTextSearch = new FullTextSearch(tests.get(i),
-        // invertedIndex, tokenizer, docsFileReader,
-        // filterHandler);
-        // HashSet<Integer> result = fullTextSearch.run();
-        // assertEquals(answers.get(i), result);
-        // }
         HashSet<Integer> expect = new HashSet<>(Arrays.asList(1, 2));
-        FullTextSearch fullTextSearch = new FullTextSearch("hello", invertedIndex, tokenizer, docsFileReader, filterHandler);
+        FullTextSearch fullTextSearch = new FullTextSearch("hello", invertedIndex, tokenizer, docsFileReader,
+                filterHandler);
         assertEquals(expect, fullTextSearch.run());
     }
 }
