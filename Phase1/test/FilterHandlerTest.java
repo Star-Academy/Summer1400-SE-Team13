63c8c9bb8d04d9626958efbfa51451c7531ac83b
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-01 22:15:56 +0430

    Revert to previous version

--- Phase1/test/FilterHandlerTest.java
+++ Phase1/test/FilterHandlerTest.java
@@ -5,32 +5,18 @@ import src.*;
 import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-
 
 public class FilterHandlerTest {
-    @Mock
-    PlusFilter plusFilter;
-    @Mock
-    MinusFilter minusFilter;
-    @Mock
-    NoSignFilter noSignFilter;
-    @Mock
-    InvertedIndex invertedIndex;
+    PlusFilter plusFilter = new PlusFilter();
+    MinusFilter minusFilter = new MinusFilter();
+    NoSignFilter noSignFilter = new NoSignFilter();
+    InvertedIndex invertedIndex = new InvertedIndex();
 
     @Before
     public void setup() {
-//        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s2", "s3")), 1);
-//        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s4", "s5")), 2);
-//        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s4", "s6")), 3);
-        Mockito.when(invertedIndex.getWordDocs("s1")).thenReturn(new HashSet<>(Arrays.asList(1,2)));
-        Mockito.when(invertedIndex.getWordDocs("s2")).thenReturn(new HashSet<>(Collections.singleton(1)));
-        Mockito.when(invertedIndex.getWordDocs("s3")).thenReturn(new HashSet<>(Collections.singleton(1)));
-        Mockito.when(invertedIndex.getWordDocs("s4")).thenReturn(new HashSet<>(Collections.singleton(2)));
-
-
-
+        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s2", "s3")), 1);
+        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s1", "s4", "s5")), 2);
+        invertedIndex.addDoc(new HashSet<>(Arrays.asList("s4", "s6")), 3);
     }
 
     @Test
