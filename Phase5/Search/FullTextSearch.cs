7f0794b68ebf171ac178ad5b93b13642eec22c02
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-03 21:58:16 +0430

    Create FullTextSearch class and test (not completely implemented)

---
+++ Phase5/Search/FullTextSearch.cs
@@ -0,0 +1,18 @@
+﻿using Search;
+
+namespace Phase5
+{
+    public class FullTextSearch
+    {
+        private readonly IInvertedIndex _invertedIndex;
+        private readonly IDocsFileReader _docsFileReader;
+        private readonly IFilterApplier _filterApplier;
+
+        public FullTextSearch(IInvertedIndex invertedIndex, IDocsFileReader docsFileReader, IFilterApplier filterApplier)
+        {
+            _invertedIndex = invertedIndex;
+            _docsFileReader = docsFileReader;
+            _filterApplier = filterApplier;
+        }
+    }
+}
---
+++ Phase5/SearchTest/FullTextSearchTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using Phase5;
+using Search;
+using Xunit;
+
+namespace SearchTest
+{
+    public class FullTextSearchTest
+    {
+        private readonly IInvertedIndex _invertedIndex;
+        private readonly IDocsFileReader _docsFileReader;
+        private readonly IFilterApplier _filterApplier;
+        private readonly FullTextSearch _fullTextSearch;
+
+        public FullTextSearchTest()
+        {
+            _invertedIndex = Substitute.For<IInvertedIndex>();
+            _docsFileReader = Substitute.For<IDocsFileReader>();
+            _filterApplier = Substitute.For<IFilterApplier>();
+            _fullTextSearch = new FullTextSearch(_invertedIndex, _docsFileReader, _filterApplier);
+        }
+    }
+}
