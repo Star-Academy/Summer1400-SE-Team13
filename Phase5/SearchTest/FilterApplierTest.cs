009b628b179c9742ce15e0adcd541871319f92f0
melika <melikahd1379@gmail.com>
2021-08-04 16:50:44 +0430

    FilterApplier implemented completely bugs fixed

--- Phase5/SearchTest/FilterApplierTest.cs
+++ Phase5/SearchTest/FilterApplierTest.cs
@@ -13,16 +13,20 @@ namespace SearchTest
         {
             _invertedIndex = Substitute.For<IInvertedIndex>();
         }
-        public void SetupInterfaces()
+        private void SetupInterfaces()
         {
-            _invertedIndex.GetWordDocs("");
+            _invertedIndex.GetWordDocs("microsoft").Returns(new HashSet<string>{"File1.txt"});
+            _invertedIndex.GetWordDocs("hello").Returns(new HashSet<string>{"File2.txt"});
+            _invertedIndex.GetWordDocs("tool").Returns(new HashSet<string>{"File3.txt"});
+            _invertedIndex.GetWordDocs("xunit").Returns(new HashSet<string>{"File3.txt"});
         }
         [Fact]
         public void FilterApplierTestMethod()
         {
+            SetupInterfaces();
             var filterApplier = new FilterApplier(_invertedIndex);
-            var testCommand = new []{"+microsoft", "hello", "-tool"};
-            var expected = new HashSet<string>(){"File1"};
+            var testCommand = new []{"+microsoft", "-xunit", "hello"};
+            var expected = new HashSet<string>(){"File2.txt"};
             Assert.Equal(expected, filterApplier.Filter(testCommand));
         }
     }
