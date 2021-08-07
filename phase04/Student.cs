6f6e6c265d15c571e6a4147e499560389875884c
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-08-07 12:47:12 +0430

    Changed student class

--- phase04/Student.cs
+++ phase04/Student.cs
@@ -1,37 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
-
-namespace phase04 
+﻿namespace ConsoleApp1
 {
-    class Student
+    public class Student
     {
-        public string FirstName {set; get;}
-        public string LastName {get; set;}
-        public int StudentNumber {get; set;}
-        private List<double> _Grades;
-        public double GPA {set; get;}
-        public Student(int studentNumber, string firstName, string lastName) {
-            GPA = 0;
-            StudentNumber = studentNumber;
-            FirstName = firstName;
-            LastName = lastName;
-            _Grades = new List<double>();
-        }
-        public void AddScore(double grade) 
-        {
-            _Grades.Add(grade);
-        }
-
-        public void CalculateGPA()
-        {
-            double gradesSum = _Grades.Sum();            
-            GPA = gradesSum/_Grades.Count;
-        }
-
-        public override string ToString()
-        {
-            return FirstName + " " + LastName + "       GPA: " + GPA + '\n'; 
-        }
+        public int StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
-
 }
