4375e88f573823c25b58de248b7e93a0893c744b
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-08-08 11:32:47 +0430

    Delete Phase04/Phase04 directory

--- Phase04/Phase04/Grade.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Phase04
-{
-    public class Grade
-    {
-        public int StudentNumber { get; set; }
-        public string Lesson { get; set; }
-        public double Score { get; set; }
-    }
-}
--- Phase04/Phase04/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Phase04
-{
-    public class Student
-    {
-        public int StudentNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
