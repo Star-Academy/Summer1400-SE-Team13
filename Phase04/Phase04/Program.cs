6e65f0d461c765bea0d9876281f4392af45d9db5
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-08-08 11:35:30 +0430

    Add files

---
+++ Phase04/Phase04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Phase04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var io = new IO();
+            var jsonConverter = new JsonConverter();
+            var studentsList = jsonConverter.GetDeserializedObjects<Student>(io.GetInput("JsonData/Students.json"));
+            var scoresList = jsonConverter.GetDeserializedObjects<Grade>(io.GetInput("JsonData/Scores.json"));
+            var scoresOrganizer = new ScoresOrganizer(studentsList, scoresList);
+            io.PrintOutput(scoresOrganizer.GetSortedGPAs(),3);
+        }
+    }
+}
