223bf674f8662fde23651532c7866aeb4fa97009
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-08 14:26:09 +0430

    Added constant file path

--- Phase04/Phase04/Program.cs
+++ Phase04/Phase04/Program.cs
@@ -2,13 +2,14 @@
 {
     class Program
     {
+        private const string PathStudents = "JsonData/Students.json";
+        private const string PathScores = "JsonData/Scores.json";
         static void Main(string[] args)
         {
-
             var io = new IO();
             var jsonConverter = new JsonConverter();
-            var studentsList = jsonConverter.GetDeserializedObjects<Student>(io.GetInput("JsonData/Students.json"));
-            var scoresList = jsonConverter.GetDeserializedObjects<Grade>(io.GetInput("JsonData/Scores.json"));
+            var studentsList = jsonConverter.GetDeserializedObjects<Student>(io.GetInput(PathStudents));
+            var scoresList = jsonConverter.GetDeserializedObjects<Grade>(io.GetInput(PathScores));
             var scoresOrganizer = new ScoresOrganizer(studentsList, scoresList);
             io.PrintOutput(scoresOrganizer.GetSortedGPAs(),3);
         }
