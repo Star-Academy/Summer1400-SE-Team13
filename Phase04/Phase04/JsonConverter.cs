0c1f53262245bea1b77b7a157be31002923cd444
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-08-08 11:30:42 +0430

    Add files via upload

---
+++ Phase04/Phase04/JsonConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Phase04
+{
+    public class JsonConverter
+    {
+        public List<T> GetDeserializedObjects<T>(string jsonFileContent)
+        {
+            return JsonSerializer.Deserialize<List<T>>(jsonFileContent);
+        }
+    }
+}
---
+++ Phase04/Phase04/ScoresOrganizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Phase04
+{
+    public class ScoresOrganizer
+    {
+        private readonly List<Student> _studentsList;
+        private readonly List<Grade> _scoresList;
+        
+        public ScoresOrganizer(List<Student> studentsList, List<Grade> scoresList){
+            _studentsList = studentsList;
+            _scoresList = scoresList;
+        }
+
+        private List<Grade> GetStudentsGrades()
+        {
+            return _studentsList.Join(_scoresList, std => std.StudentNumber, grade => grade.StudentNumber, (std, grade) => grade).ToList();
+        }
+
+        private Dictionary<Student, double> SetStudentAverage()
+        {
+            var studentsGrades = GetStudentsGrades();
+            var studentAverage = _studentsList.Select(s =>
+                new {
+                Student = s,
+                Average = studentsGrades.Where(p => p.StudentNumber == s.StudentNumber).Select(x => x.Score).Average()
+                 }).ToDictionary(x => x.Student, x => x.Average);
+            return studentAverage;
+        }
+
+        public Dictionary<Student, double> GetSortedGPAs()
+        {
+            return SetStudentAverage().OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
