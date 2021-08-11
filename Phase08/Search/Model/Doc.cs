aac57e138a053922a0a45cf7121d0da1283a18d6
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-11 10:17:48 +0430

    Change key type to int

--- Phase08/Search/Model/Doc.cs
+++ Phase08/Search/Model/Doc.cs
@@ -6,7 +6,7 @@ namespace Search.Model
     public class Doc
     {
         [Key]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Content { get; set; }
         public List<Word> Words { get; set; }
--- Phase08/Search/Model/Word.cs
+++ Phase08/Search/Model/Word.cs
@@ -6,7 +6,7 @@ namespace Search.Model
     public class Word
     {
         [Key]
-        public string Id {get; set;}
+        public int Id {get; set;}
         public string Content {get; set;}
         public List<Doc> Docs {get; set;}
     }
