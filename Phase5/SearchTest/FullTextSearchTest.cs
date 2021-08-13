827c55d4c8928a7ebc6e2cfc6853159f91491adf
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-13 15:31:51 +0430

    used constant variable + deleted useless method

--- Phase5/SearchTest/FullTextSearchTest.cs
+++ Phase5/SearchTest/FullTextSearchTest.cs
@@ -7,7 +7,6 @@ namespace SearchTest
 {
     public class FullTextSearchTest
     {
-        private readonly IDocsFileReader _docsFileReader;
         private readonly IFilterApplier _filterApplier;
         private readonly IQueryProcessor _queryProcessor;
         private readonly IFullTextSearch _fullTextSearch;
@@ -17,15 +16,15 @@ namespace SearchTest
             var invertedIndex = Substitute.For<IInvertedIndex>();
             var tokenizer = Substitute.For<ITokenizer>();
             _queryProcessor = Substitute.For<IQueryProcessor>();
-            _docsFileReader = Substitute.For<IDocsFileReader>();
+            var docsFileReader = Substitute.For<IDocsFileReader>();
             _filterApplier = Substitute.For<IFilterApplier>();
-            _fullTextSearch = new FullTextSearch(invertedIndex, _docsFileReader,tokenizer, _queryProcessor, _filterApplier);
+            _fullTextSearch = new FullTextSearch(invertedIndex, docsFileReader,tokenizer, _queryProcessor, _filterApplier);
         }
 
         [Fact]
         public void TestSearchQueryWithOneNoSignWord()
         {
-            SetupInterfaces();
+            SetupMockedFilterApplier();
             const string query = "hello";
             _queryProcessor.MinusCommandWords.Returns(new HashSet<string>());
             _queryProcessor.PlusCommandWords.Returns(new HashSet<string>());
@@ -38,8 +37,8 @@ namespace SearchTest
         [Fact]
         public void TestSearchQueryWithNoSignPlusMinusWords()
         {
-            SetupInterfaces();
-            var query = "hello +java -sample";
+            SetupMockedFilterApplier();
+            const string query = "hello +java -sample";
             _queryProcessor.MinusCommandWords.Returns(new HashSet<string>(){"sample"});
             _queryProcessor.PlusCommandWords.Returns(new HashSet<string>(){"java"});
             _queryProcessor.NoSignCommandWords.Returns(new HashSet<string>(){"hello"});
@@ -51,8 +50,8 @@ namespace SearchTest
         [Fact]
         public void TestSearchQueryWithNoSignAndMinusWords()
         {
-            SetupInterfaces();
-            var query = "-hello java";
+            SetupMockedFilterApplier();
+            const string query = "-hello java";
             _queryProcessor.MinusCommandWords.Returns(new HashSet<string>(){"hello"});
             _queryProcessor.PlusCommandWords.Returns(new HashSet<string>());
             _queryProcessor.NoSignCommandWords.Returns(new HashSet<string>(){"java"});
@@ -61,21 +60,7 @@ namespace SearchTest
             Assert.Equal(expectedResult, actualResult);
         }
         
-        private void SetupInterfaces()
-        {
-            SetupDocsFileReader();
-            SetupFilterApplier();
-        }
-        private void SetupDocsFileReader()
-        {
-            _docsFileReader.ReadContent().Returns(new Dictionary<string, string>
-            {
-                {"File1","Hello Java."},
-                {"File2", "Hello, python"},
-                {"File3", "sample Java"}
-            });
-        }
-        private void SetupFilterApplier()
+        private void SetupMockedFilterApplier()
         {
             _filterApplier.Filter(Arg.Is<HashSet<string>>(x => x.Count == 1),Arg.Is<HashSet<string>>(x => x.Count == 1),Arg.Is<HashSet<string>>(x => x.Count == 1)).Returns(new HashSet<string>(){"File1"});
             _filterApplier.Filter(Arg.Is<HashSet<string>>(x => x.Count == 0),Arg.Is<HashSet<string>>(x => x.Count == 0),Arg.Is<HashSet<string>>(x => x.Count == 1)).Returns(new HashSet<string>(){"File1", "File2"});
