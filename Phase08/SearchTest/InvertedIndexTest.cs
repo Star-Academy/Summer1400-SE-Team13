8d9a006c455813d4a0fdf04f26e37c38ee97ee28
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-14 14:19:36 +0430

    Fixed bug

--- Phase08/SearchTest/InvertedIndexTest.cs
+++ Phase08/SearchTest/InvertedIndexTest.cs
@@ -1,44 +1,49 @@
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
 using Search;
 using Search.Model;
 using Xunit;
 using NSubstitute;
-using NSubstitute.ClearExtensions;
-
 
 namespace SearchTest
 {
     public class GetWordDocsTest
     {
-        private HashSet<Doc> _docs;
-        private ITokenizer _tokenizer;
-        private InvertedIndex _invertedIndex;
+        private readonly ITokenizer _tokenizer;
+        private IInvertedIndex _invertedIndex;
 
         public GetWordDocsTest()
         {
-            _docs = new HashSet<Doc>();
             _tokenizer = Substitute.For<ITokenizer>();
         }
-        HashSet<Doc> SetupDocs()
+        
+        [Fact]
+        public void BuildInvertedIndexTest()
         {
-            _docs.Add(new Doc()
+            SetupMockedTokenizer();
+            using var context = new SearchContext();
+            _invertedIndex = new InvertedIndex(context);
+            _invertedIndex.BuildInvertedIndex(SetupDocs(), _tokenizer);
+            var expected = new HashSet<string>() {"File1"};
+            Assert.Equal(expected, _invertedIndex.GetWordDocs("microsoft"));
+        }
+        private HashSet<Doc> SetupDocs()
+        {
+            var docs = new HashSet<Doc>()
             {
-                Name = "File1.txt",
-                Content = "Microsoft have just announced collaborative coding via Live Share."
-            });
-            _docs.Add(new Doc()
-            {
-                Name = "File2.txt",
-                Content = "Hello World!"
-            });
-            _docs.Add(new Doc()
-            {
-                Name = "File3.txt",
-                Content = "Hello, Xunit is a free and open-source unit testing tool."
-            });
-            return _docs;
+                new()
+                {
+                    Name = "File1", Content = "Microsoft have just announced collaborative coding via Live Share."
+                },
+                new()
+                {
+                    Name = "File2", Content = "Hello World!"
+                },
+                new()
+                {
+                    Name = "File3", Content = "Hello, Xunit is a free and open-source unit testing tool."
+                }
+            };
+            return docs;
         }
 
         private void SetupMockedTokenizer()
@@ -48,17 +53,6 @@ namespace SearchTest
             _tokenizer.Tokenize("Hello World!").Returns(new HashSet<string>(){"hello", "world"});
             _tokenizer.Tokenize("Hello, Xunit is a free and open-source unit testing tool.")
                 .Returns(new HashSet<string>(){"hello", "xunit", "is", "free", "and", "open", "source", "unit", "testing", "tool"});
-        }
-        
-        [Fact]
-        public void BuildInvertedIndexTest()
-        {
-            SetupDocs();
-            SetupMockedTokenizer();
-            using var context = new SearchContext();
-            _invertedIndex = new InvertedIndex(context);
-            HashSet<string> expected = new HashSet<string>() { "File1" };
-            Assert.Equal(_invertedIndex.GetWordDocs("microsoft"), expected);
         }
     }
 }
