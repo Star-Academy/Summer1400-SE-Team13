9263a681e03137c70d8c03d1024b16870b4fa1b9
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-08-15 13:01:57 +0430

    Migration added

---
+++ Phase08/Search/Migrations/20210815065454_InvertedIndex.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Search.Migrations
+{
+    public partial class InvertedIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Docs",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Docs", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Content = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Content);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelationTable",
+                columns: table => new
+                {
+                    DocsName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    WordsContent = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelationTable", x => new { x.DocsName, x.WordsContent });
+                    table.ForeignKey(
+                        name: "FK_RelationTable_Docs_DocsName",
+                        column: x => x.DocsName,
+                        principalTable: "Docs",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RelationTable_Words_WordsContent",
+                        column: x => x.WordsContent,
+                        principalTable: "Words",
+                        principalColumn: "Content",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelationTable_WordsContent",
+                table: "RelationTable",
+                column: "WordsContent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RelationTable");
+
+            migrationBuilder.DropTable(
+                name: "Docs");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
