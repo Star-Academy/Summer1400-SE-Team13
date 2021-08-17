b3b6acfa1764f78514b929010da7013a8f7e7101
Raha Ahmadi <58840766+rahaahmadi@users.noreply.github.com>
2021-08-17 11:38:52 +0430

    Corrected typo

--- Phase5/Search/IOHandler.cs
+++ Phase5/Search/IOHandler.cs
@@ -17,7 +17,7 @@ namespace Phase5
          {
              if (!commandResult.Any())
              {
-                 Console.WriteLine("No do found!");
+                 Console.WriteLine("No doc found!");
              }
              else
              {
